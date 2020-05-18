2b011e4bf83196ce9363e53b8fec4a60a11b56b8
baiello <baiello@users.noreply.github.com>
2020-05-18 18:53:05 +0000

    deployed via Gatsby Publish Action 🎩 for b6081edfa76884348c6a18e4a96fc005b006fd62

---
+++ component---src-pages-404-js-93753a6ae7a7c5d62c3d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"Page Not found"}),l.a.createElement("h1",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-93753a6ae7a7c5d62c3d.js.map
---
+++ component---src-pages-contact-js-dd486a5d43e4e17e6658.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"Cuy+":function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),c=n.n(a),r=n("Bl7J"),u=n("vrFN");e.default=function(){return c.a.createElement(r.a,null,c.a.createElement(u.a,{title:"Contact"}))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-dd486a5d43e4e17e6658.js.map
